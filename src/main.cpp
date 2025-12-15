6ac44cac65d82e85fd12c07e72872bf282448037
sektant1 <fergabe@pm.me>
2025-12-15 10:53:00 -0300

    setup project and create window

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+auto main() -> int
+{
+    if (!glfwInit()) {
+        std::cout << "Failed initializing glfw" << "\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window =
+        glfwCreateWindow(1280, 720, "OpenGL Game", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Error creating window" << "\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetWindowPos(window, 256, 256);
+    glfwMakeContextCurrent(window);
+
+    if (!glewInit() != GLEW_OK) {
+        std::cout << "Failed initializing glew" << "\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
